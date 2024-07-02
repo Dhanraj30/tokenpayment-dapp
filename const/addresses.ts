4f76be4d8d7ebf9eebf8b4c60131b62cf0371c51
Dhanraj Avhad <dhanraj30102002@gmail.com>
2024-07-02 13:17:59 +0530

    first commit

---
+++ const/addresses.ts
@@ -0,0 +1,7 @@
+export const TRANSFER_CONTRACT_ADDRESS = '0x295c96a0E1EC27C3eC0cA2fd575dbcd4a58c036c';
+export const CLAIM_TOKEN_CONTRACT_ADDRESS = '0x5c4E150a9cf6672D70799aB329239738d6309d5b';//'0x534DFf2653122acb67BB3F942990FEFf916cc14D';//'0x09043753e8b0B95d526e22cd67A3bA34449B0764';
+
+
+export const HERO_IMAGE_URL = 'ipfs://QmUWVRuyFuVfo45estsJt2VaQUSEc4uwcPoeWFaeYxERua/seanwatase_isometric_crypto_transferring_from_one_wallet_to_ano_96084ff1-fe03-4dfd-8783-a3ecc85a24da.png';
+export const FEATURES_IMAGE_URL = 'ipfs://QmcTgfb2AXj1TcVj4i9jSgZc8A8LhxHYCXbu5Mngsx7iVT/Coins.png';
+export const CLAIM_TOKEN_IMAGE = 'ipfs://QmUYpRzQE6bebNuuVriyd2n163wAKu1pNRusqQraeRU5bX/seanwatase_isometric_black_crypto_coin_transparent_background_3dcbfe44-babc-45f5-9d43-881d85d2293d.png';
